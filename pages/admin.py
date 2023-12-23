a9660112b465496e724c04c286f9aa8a4bb678eb
aslansinan <aslansinan586@gmail.com>
2023-12-23 16:11:32 +0300

    fix.

--- pages/admin.py
+++ pages/admin.py
@@ -5,9 +5,8 @@ from pages.models import Urun
 
 # Register your models here.
 class UrunAdmin(admin.ModelAdmin):
-    list_display = ['kod', 'iism']
+    list_display = ['kod', 'isim','fiyat']
     search_fields = ['isim']
-
 
 
 admin.site.register(Urun, UrunAdmin)
