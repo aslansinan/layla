23055090afe774ef83ca720574ca0ac58d51303d
aslansinan <aslansinan586@gmail.com>
2024-01-28 12:33:50 +0300

    basic code denemesi

--- payment/views.py
+++ payment/views.py
@@ -1,19 +1,26 @@
-from django.shortcuts import render
+from datetime import datetime
+from django.utils import timezone
+from django.core.serializers.json import DjangoJSONEncoder
+from django.shortcuts import render, get_object_or_404
 import iyzipay
 import json
-# Create your views here.
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseServerError
 from django.urls import reverse
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
-import requests
-from django.contrib import messages
-import pprint
+from django.contrib.auth.decorators import login_required
 
-api_key = 'sandbox-etkBOaBAec7Zh6jLDL59Gng0xJV2o1tV'
-secret_key = 'sandbox-uC9ysXfBn2syo7ZMOW2ywhYoc9z9hTHh'
+from account.models import UyeAdresi, Uye
+from pages.models import Kategori
+from satis.models import Sepet, Siparis, SiparisSatiri
+import logging
+import random
+logger = logging.getLogger(__name__)
+
+
+api_key = 'sandbox-F4nYkhziRRvqnjhMV9u0IQMrekBwalBk'
+secret_key = 'sandbox-DxkGCnUj3dH9hglAI9MZrU7T4uFdd1jg'
 base_url = 'sandbox-api.iyzipay.com'
-
 
 options = {
     'api_key': api_key,
@@ -21,115 +28,148 @@ options = {
     'base_url': base_url
 }
 sozlukToken = list()
-
+random_value = random.randint(100000000, 999999999)
+conversationId = random_value
 
 def index(request):
     context = dict()
 
     context['message'] = 'Django ile Iyzico Ödeme Entegrasyonu'
 
-
     template = 'payment/index.html'
     return render(request, template, context)
 
 
+class KategoriEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Kategori):
+            # Convert your Kategori object to a dictionary or any serializable format
+            return {
+                'id': obj.id,
+                'name': obj.baslik,
+            }
+        return super().default(obj)
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
 def payment(request):
+    selected_address_id = request.POST.get('selectedAddress')
+    # Do something with the selected_address_id, such as retrieving the address from the database
+    selected_address = UyeAdresi.objects.get(id=selected_address_id)
+    user = request.user
+    selected_user = Uye.objects.get(email=user.email)
+    user_ip = get_client_ip(request)
+    # Rest of your payment logic goes here
+    user_cart = get_object_or_404(Sepet, user=user, durum=Sepet.HAZIRLIKTA)
+    cart_items = user_cart.sepetsatiri_set.all()
     context = dict()
-
-    buyer={
-        'id': 'BY789',
-        'name': 'John',
-        'surname': 'Doe',
-        'gsmNumber': '+905350000000',
-        'email': 'email@email.com',
-        'identityNumber': '74300864791',
-        'lastLoginDate': '2015-10-05 12:43:35',
-        'registrationDate': '2013-04-21 15:12:09',
-        'registrationAddress': 'Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1',
-        'ip': '85.34.78.112',
-        'city': 'Istanbul',
+    buyer = {
+        'id': selected_user.id,
+        'name': selected_user.isim,
+        'surname': selected_user.soyisim,
+        'gsmNumber': selected_address.tel,
+        'email': selected_user.email,
+        'identityNumber': selected_user.tc,
+        'lastLoginDate': user.date_joined.strftime('%Y-%m-%d %H:%M:%S'),
+        'registrationDate': user.date_joined.strftime('%Y-%m-%d %H:%M:%S'),
+        'registrationAddress': selected_address.adres,
+        'ip': user_ip,
+        'city': selected_address.il,
         'country': 'Turkey',
-        'zipCode': '34732'
+        'zipCode': selected_address.posta_kodu
     }
 
-    address={
-        'contactName': 'Jane Doe',
-        'city': 'Istanbul',
+    address = {
+        'contactName': selected_address.isim,
+        'city': selected_address.il,
         'country': 'Turkey',
-        'address': 'Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1',
-        'zipCode': '34732'
+        'address': selected_address.adres,
+        'zipCode': selected_address.posta_kodu
     }
-
-    basket_items=[
-        {
-            'id': 'BI101',
-            'name': 'Binocular',
-            'category1': 'Collectibles',
-            'category2': 'Accessories',
-            'itemType': 'PHYSICAL',
-            'price': '0.3'
-        },
-        {
-            'id': 'BI102',
-            'name': 'Game code',
-            'category1': 'Game',
-            'category2': 'Online Game Items',
-            'itemType': 'VIRTUAL',
-            'price': '0.5'
-        },
-        {
-            'id': 'BI103',
-            'name': 'Usb',
-            'category1': 'Electronics',
-            'category2': 'Usb / Cable',
-            'itemType': 'PHYSICAL',
-            'price': '0.2'
+    basket_items = []
+    total_amount = 0
+    for cart_item in cart_items:
+        basket_item = {
+            'id': cart_item.urun.id,  # Use the appropriate field for the product ID
+            'name': cart_item.urun.isim,
+            'category1': KategoriEncoder().encode(cart_item.urun.kategori),  # Adjust as per your model structure
+            'itemType': 'PHYSICAL',  # Adjust as per your model structure
+            'price': str(cart_item.urun.fiyat * cart_item.adet),  # Convert the price to a string
         }
-    ]
+        basket_items.append(basket_item)
+        total_amount += cart_item.fiyat
+    total_amount = float(total_amount)
 
-    request={
+    payment_request = {
         'locale': 'tr',
-        'conversationId': '123456789',
-        'price': '1',
-        'paidPrice': '1.2',
+        'conversationId': str(conversationId),
+        'price': total_amount,
+        'paidPrice': total_amount,
         'currency': 'TRY',
-        'basketId': 'B67832',
+        'basketId': user_cart.id,
         'paymentGroup': 'PRODUCT',
-        "callbackUrl": "https://www.laylabutik.com/payment/result/",
+        "callbackUrl": "http://localhost:8000/payment/result/",
         "enabledInstallments": ['2', '3', '6', '9'],
         'buyer': buyer,
         'shippingAddress': address,
         'billingAddress': address,
         'basketItems': basket_items,
+        'userId': user_cart.id
         # 'debitCardAllowed': True
     }
+    request.session['user_id'] = selected_user.id
 
-    checkout_form_initialize = iyzipay.CheckoutFormInitialize().create(request, options)
+    try:
+        checkout_form_initialize = iyzipay.CheckoutFormInitialize().create(payment_request, options)
+        content = checkout_form_initialize.read().decode('utf-8')
+        json_content = json.loads(content)
+        print(type(json_content))
+        print(json_content["checkoutFormContent"])
+        print(json_content["token"])
+        token = json_content.get("token")
+        print(token)
 
-    #print(checkout_form_initialize.read().decode('utf-8'))
-    page = checkout_form_initialize
-    header = {'Content-Type': 'application/json'}
-    content = checkout_form_initialize.read().decode('utf-8')
-    json_content = json.loads(content)
-    print(type(json_content))
-    print(json_content["checkoutFormContent"])
-    print("************************")
-    print(json_content["token"])
-    print("************************")
-    sozlukToken.append(json_content["token"])
-    return HttpResponse(json_content["checkoutFormContent"])
+        if token:
+            print(token)
+            sozlukToken.append(json_content["token"])
+            logger.info("Token added to sozlukToken: %s", json_content["token"])
+            return HttpResponse(json_content["checkoutFormContent"])
+        else:
+            # Token alınamadıysa
+            print("Error: Token not found in the response")
+            logger.error("Error: Token not found in the response")
+            # Handle this case as needed, maybe redirect to an error page
+            return HttpResponse("Error: Token not found in the response")
+
+    except json.decoder.JSONDecodeError as e:
+        print("Error decoding JSON:", e)
+        logger.exception("An error occurred during payment")
+        print("Response content:", content)
+        # Handle the error as needed
+        return HttpResponse("Error decoding JSON")
+    except Exception as e:
+        print("An error occurred during payment:", e)
+        # Handle other exceptions as needed
+        return HttpResponseServerError("Internal Server Error during payment")
+
 
 
 @require_http_methods(['POST'])
 @csrf_exempt
 def result(request):
     context = dict()
-
     url = request.META.get('index')
-
     request = {
         'locale': 'tr',
-        'conversationId': '123456789',
+        'conversationId': str(conversationId),
         'token': sozlukToken[0]
     }
     checkout_form_result = iyzipay.CheckoutForm().retrieve(request, options)
@@ -137,41 +177,118 @@ def result(request):
     print(type(checkout_form_result))
     result = checkout_form_result.read().decode('utf-8')
     print("************************")
-    print(sozlukToken[0])   # Form oluşturulduğunda
+    # print(sozlukToken[0])  # Form oluşturulduğunda
     print("************************")
     print("************************")
     sonuc = json.loads(result, object_pairs_hook=list)
-    #print(sonuc[0][1])  # İşlem sonuç Durumu dönüyor
-    #print(sonuc[5][1])   # Test ödeme tutarı
+    # print(sonuc[0][1])  # İşlem sonuç Durumu dönüyor
+    # print(sonuc[5][1])   # Test ödeme tutarı
     print("************************")
     for i in sonuc:
         print(i)
     print("************************")
-    print(sozlukToken)
+    # print(sozlukToken)
     print("************************")
     if sonuc[0][1] == 'success':
         context['success'] = 'Başarılı İŞLEMLER'
-        return HttpResponseRedirect(reverse('success'), context)
+        success_url = reverse('success') + f'?sonuc={sonuc}'
+        return HttpResponseRedirect(success_url)
+
+
 
     elif sonuc[0][1] == 'failure':
         context['failure'] = 'Başarısız'
-        return HttpResponseRedirect(reverse('failure'), context)
+        success_url = reverse('failure')
+        return HttpResponseRedirect(success_url)
 
     return HttpResponse(url)
 
+def update_cart_status(user_cart, payment_id):
+    user_cart.durum = Sepet.TAMAMLANDI  # Assuming you have a constant like TAMAMLANDI for completed status
+    user_cart.guncelleme_tarihi = datetime.now()
+    user_cart.order_id = payment_id
+    user_cart.save()
+    return
+
+
+def move_cart_items_to_order(user_cart, order):
+    cart_items = user_cart.sepetsatiri_set.all()
+    for cart_item in cart_items:
+        order_item = SiparisSatiri.objects.create(
+            siparis=order,
+            urun=cart_item.urun,
+            adet=cart_item.adet,
+            birim_fiyat=cart_item.urun.fiyat,
+            durum='H'
+        )
+        # Adjust stock quantity
+        cart_item.urun.stok_durumu -= cart_item.adet
+        cart_item.urun.save()
+    return
+
+
+def create_order(user, user_cart, payment_id, card_type, tutar):
+    odeme_tipi_mapping = {
+        'CREDIT_CARD': 'K',
+        'Havale': 'H',
+        'KAPIDA_ODEME_KREDI_KARTI': 'KK',
+        'KAPIDA_ODEME_NAKIT': 'KN',
+    }
+    sepet_instance = get_object_or_404(Sepet, id=user_cart.id)
+    print(sepet_instance)
+    order = Siparis.objects.create(
+        user=user,
+        sepet=sepet_instance,
+        tarih=timezone.now(),
+        order_id=payment_id,
+        odeme_tipi=odeme_tipi_mapping.get(card_type, ''),
+        durum='S',
+        toplam_tutar=tutar,
+        kargo_ucreti=50
+    )
+    return order
 
 
 def success(request):
     context = dict()
+    template = 'payment/ok.html'
     context['success'] = 'İşlem Başarılı'
+    user = request.user
+    sonuc_str = request.GET.get('sonuc', None)
+    if sonuc_str:
+        sonuc_str = sonuc_str[1:-1]
 
-    template = 'ok.html'
-    return render(request, template, context)
+        # Tuple'ları içeren string'i ayırın
+        tuple_str_list = sonuc_str.split("), (")
+
+        # Her bir tuple'ı bir string olarak ele alıp, uygun bir formata getirin
+        for i in range(len(tuple_str_list)):
+            tuple_str_list[i] = tuple_str_list[i].replace("(", "").replace(")", "")
+
+        # Her bir tuple string'ini bir tuple nesnesine dönüştürün
+        sonuc_list = [tuple(item.split(", ")) for item in tuple_str_list]
+
+        # Şimdi, sonuc_list içinde tuple'lar içeren bir liste elde ettiniz
+
+        user_cart = get_object_or_404(Sepet, user=user, durum=Sepet.HAZIRLIKTA)
+        temp =sonuc_list[7][1]
+        temp = temp.strip("'")
+        temp1 =sonuc_list[5][1]
+        temp1 = temp1.strip("'")
+        value_float = int(float(temp))
+        value_float1 = int(float(temp1))
+        order = create_order(user, user_cart, payment_id=value_float, card_type=sonuc_list[13][1],
+                             tutar=value_float1, )
+        move_cart_items_to_order(user_cart, order)
+        update_cart_status(user_cart, payment_id=value_float)
+        return render(request, template, context)
+    else:
+        return render(request, template, context)
 
 
 def fail(request):
     context = dict()
     context['fail'] = 'İşlem Başarısız'
 
-    template = 'fail.html'
+    template = '/fail.html'
     return render(request, template, context)
